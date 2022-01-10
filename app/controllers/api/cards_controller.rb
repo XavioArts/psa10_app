2f7c4560d629cffe65dc7dbbfd6417ee61b01be1
Austin <austinchoi22@gmail.com>
2022-01-10 16:21:41 -0700

    cards controller

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,2 +1,48 @@
 class Api::CardsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_card, only: [:show, :destroy, :update]
+
+  def all_cards
+    render json: Card.all
+  end
+
+  def index 
+    render json: current_user.cards
+  end
+
+  def show
+    render json: @Card
+  end
+
+  def create
+    @card = current_user.cards.new(card_params)
+    if @card.save
+      render json: @card
+    else 
+      render json: {error: @card.errors}, status: 422
+    end
+  end
+
+  def update
+    if @card.update(card_params)
+      render json: @card
+    else
+      render json: {error: @card.errors}, status: 422
+    end
+  end
+
+  def destroy
+    render json: @card.destroy
+  end
+
+  private
+
+  def set_card
+    @card = current_user.cards.find(params[:id, :user_id, :collection_id])
+  end
+
+  def card_params
+    params.requre(:card).permit(:name, :price, :description, :condition, :sale, :trade, :likes, :front_image, :back_image )
+  end
+
 end
