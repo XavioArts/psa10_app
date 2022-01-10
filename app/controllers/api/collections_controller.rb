94f2473db204b5de703855b29efc5ae0ce58182c
Fabio Xavier <fabio.g.xavier33@gmail.com>
2022-01-10 16:53:40 -0700

    collections controller filled out (not tested)

--- app/controllers/api/collections_controller.rb
+++ app/controllers/api/collections_controller.rb
@@ -1,2 +1,49 @@
 class Api::CollectionsController < ApplicationController
+
+    before_action :authenticate_user!, except: [:all_collections]
+    before_action :set_collection, only: [:destroy, :update, :show]
+
+    def index
+        render json: current_user.collections
+    end
+
+    def all_collections
+        render json: Collection.all
+    end
+
+    def show
+        render json: @collection
+    end
+
+    def create
+        @collection = current_user.collections.new(collection_params)
+        if @collection.save
+            render json: @collection 
+        else
+            render json: {errors: @collection.errors}, status: 422
+        end
+    end
+
+    def update
+        if @collection.update(collection_params)
+            render json: @collection
+        else
+            render json: {errors: @collection.errors}, status: 422
+        end
+    end
+
+    def destroy
+        render json: @collection.destroy
+    end
+
+    private
+
+    def collection_params
+        params.require(:collection).permit(:category, :name, :description, :likes, :user_id)
+    end
+
+    def set_collection
+        @collection = Collection.find(:id)
+    end
+
 end
