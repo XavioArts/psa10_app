cd70caa960ba9c5f9198d57fc76dc5782f21c5df
Fabio Xavier <fabio.g.xavier33@gmail.com>
2022-01-12 10:21:22 -0700

    navigate to home after image upload

--- client/src/components/ProfileImageUpload.js
+++ client/src/components/ProfileImageUpload.js
@@ -1,12 +1,15 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../providers/AuthProvider";
-import { Button, Icon, Input } from "@mui/material";
+import { Alert, Button, Icon, Input } from "@mui/material";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const ProfileImageUpload = () => {
 
     const auth = useContext(AuthContext);
     const [files, setFiles] = useState([]);
+    const [success, setSuccess] = useState(false);
+    const navigate = useNavigate();
 
     const handleUpload = async (e) => {
         e.preventDefault();
@@ -17,6 +20,9 @@ const ProfileImageUpload = () => {
         try {
             let res = await axios.post('/api/users/image', data);
             console.log(res.data)
+            setSuccess(true);
+            auth.setUser(res.data);
+            navigate("/");
         } catch (err) {
             console.log(err.response);
             alert("there was an error uploading")
@@ -26,6 +32,7 @@ const ProfileImageUpload = () => {
     return (
         <div>
             <h3>Update your profile image</h3>
+            {success && <Alert severity="success" >Successfully updated profile picture!</Alert>}
             {auth.image === null && 
                 <div style={{width: "200px", height: "200px", background: "#D7D7D7", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}} >
                     <Icon sx={{fontSize: "115px", margin: "0"}} >
@@ -39,8 +46,8 @@ const ProfileImageUpload = () => {
                 <Input accept="image/*" value={files} type="file" id="input" onChange={(e)=>setFiles(e.target.value)} />
                 <Button variant="contained" component="span" endIcon={<Icon>photocamera</Icon>} onClick={handleUpload} >Upload</Button>
             </label>
-                <Button variant="contained" onClick={()=>console.log(files)} >Log Files</Button>
-                <Button variant="contained" onClick={()=>console.log(document.getElementById("input").files[0])} >get file</Button>
+                {/* <Button variant="contained" onClick={()=>console.log(files)} >Log Files</Button>
+                <Button variant="contained" onClick={()=>console.log(document.getElementById("input").files[0])} >get file</Button> */}
         </div>
     )
 }
