1ad698c23fb8e1dcf95ddd4ad73243fb9d8899dc
Sky Lin <linsky519@gmail.com>
2022-01-13 16:30:16 -0700

    hard coded collection card, need to fix it

--- client/src/pages/Protected.js
+++ client/src/pages/Protected.js
@@ -1,27 +1,51 @@
 import { Alert } from "@mui/material";
-import React, { useContext } from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, useNavigate, Outlet } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
+import { DateTime } from "luxon";
+
 
 const Protected = () => {
+    const [user, setUser] = useState([])
+
+    useEffect(() => {
+        userInfo();
+    }, [])
+
+    const userInfo = async () => {
+        let res = await axios.get(`/api/users/${auth.id}`)
+        setUser(res.data)
+        console.log(res.data)
+        console.log(auth.id)
+    }
 
     const navigate = useNavigate();
     const auth = useContext(AuthContext);
 
     return (
         <div>
-            {!auth.image && <Alert severity="error" >Finish building your profile. <button onClick={()=>navigate(`/users/${auth.id}/edit`)}>Edit Profile</button></Alert>}
+            {!auth.image && <Alert severity="error" >Finish building your profile. <button onClick={() => navigate(`/users/${auth.id}/edit`)}>Edit Profile</button></Alert>}
             <h2>My Profile</h2>
-            <h3>Hey, {auth.nickname}!</h3>
-            {auth.image && <img src={auth.image} alt="profile image" width="200px"/>}
-            <button onClick={()=>navigate("/profile_image")} >Edit profile image</button>
+            {auth.image && <img src={auth.image} alt="profile image" width="200px" />}
+            <button onClick={() => navigate("/profile_image")} >Edit profile image</button>
+            <h2>{auth.nickname}</h2>
+            <p>Joined: {DateTime.fromISO(user.created_at).toFormat('LLLL yyyy')}</p>
+            <p>Email: {auth.email}</p>
             <p>About Me: {auth.about}</p>
             <p>Name: {auth.first_name} {auth.last_name}</p>
-            <p>Email: {auth.email}</p>
-            <p>My ID {auth.id}</p>       
+            <p>My ID {auth.id}</p>
             <Link to={`/users/${auth.id}/edit`}>Edit Profile</Link>
-            <hr/>
+            <hr />
+            <div>
+                <a href="/profile">Overview</a>
+                <a href="/profile/collections">Collections</a>
+                <a href="/sets">Sets</a>
+                <a href="/showcases">Showcases</a>
+            </div>
+            <hr />
             <Outlet />
+
         </div>
     );
 };
