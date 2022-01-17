460ef82166e01cf142c9474da8f28ca29f6cafcf
Fabio Xavier <fabio.g.xavier33@gmail.com>
2022-01-17 00:46:28 -0700

    search bar stubbed out

--- client/src/pages/Community.js
+++ client/src/pages/Community.js
@@ -1,3 +1,4 @@
+import { Button, Icon, Input } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -24,6 +25,13 @@ const Community = () => {
     return (
         <div>
             <h1>Community</h1>
+            <h3>Find a collector</h3>
+            <div style={{width: "75vw", margin: "auto", padding: "10px", display: "flex", justifyContent: "center", alignItems: "center"}} >
+                <div style={{width: "50vw", margin: "auto", padding: "10px"}} >
+                <Input fullWidth startAdornment={<Icon>search</Icon>} placeholder="Find a collector.." type="search" />
+                </div>
+                <Button variant="contained" >Search</Button>
+            </div>
             <div style={{width: "85vw", margin: "auto", padding: "10px"}} >
                 <code style={{overflowWrap: "break-word"}} >{JSON.stringify(users)}</code>
             </div>
