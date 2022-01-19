62caecb3499ce9d004455c490b52d5471e0a93a9
John Leosco <jrleosco@gmail.com>
2022-01-19 12:18:52 -0700

    topic page added

--- client/src/pages/MessageBoard.js
+++ client/src/pages/MessageBoard.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Box from '@mui/material/Box';
 import { Button } from "@mui/material";
 import Modal from '@mui/material/Modal';
 import AddTopic from '../components/AddTopic';
+import { AuthContext } from '../providers/AuthProvider';
 
 const MessageBoard = (props) => {
+  const auth = useContext(AuthContext);
   const [topics, setTopics] = useState([])
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
@@ -16,7 +18,7 @@ const MessageBoard = (props) => {
 
   useEffect(()=>{
     getTopics()
-  },[])
+  },[open])
 
   const getTopics = async () => {
     try {
@@ -33,16 +35,23 @@ const MessageBoard = (props) => {
     handleClose()
   }
 
+  const deleteTopic = async (id) => {
+    await axios.delete(`/api/topics/${id}`)
+    const filteredTopics = topics.filter((topic) => topic.id !== id)
+    setTopics(filteredTopics)
+}
+
   const renderTopics = (topics) => {
     let topicBox = topics.map(t=>{
       const handleOnClick = () =>{
         navigate(`/messageboard/${t.id}`)
       }
       return(
-        <Box key={t.id} onClick={()=>handleOnClick()} style={{ padding: '5px', border: '1px solid grey', borderRadius: '10px', margin: '20px' }}>
+        <Box key={t.id} style={{ padding: '5px', border: '1px solid grey', borderRadius: '10px', margin: '20px' }}>
           <h6 style={{margin: '5px'}}>Posted by {t.user_nickname}</h6>
-          <h3>{t.title}</h3>
+          <h3 onClick={()=>handleOnClick()}>{t.title}</h3>
           <p>{t.body}</p>
+          {auth.id === t.user_id && <Button variant="contained" style={{ backgroundColor: 'red'}} onClick={() => deleteTopic(t.id)}>Delete Topic</Button>}
         </Box>
       )
     })
--- client/src/pages/TopicPage.js
+++ client/src/pages/TopicPage.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+import { Button } from "@mui/material";
+import Modal from '@mui/material/Modal';
+import EditTopic from '../components/EditTopic';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const TopicPage = () => {
+  const auth = useContext(AuthContext);
+  const params = useParams();
+  const [topic, setTopic] = useState([])
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  useEffect(()=>{
+    getTopic()
+  },[])
+
+  const getTopic = async () => {
+    try {
+      let res = await axios.get(`/api/topics/${params.id}`);
+      setTopic(res.data);
+    } catch (err) {
+      console.log(err.response);
+      alert("Error getting topic")
+    }
+  }
+
+
   return (
-    <div>Topic</div>
+    <div>
+      <h1>{topic.title}</h1>
+      <p>{topic.body}</p>
+        <Button variant="contained" onClick={handleOpen}>Edit Topic</Button>
+        <Modal open={open} onClose={handleClose}>
+          <div><EditTopic /></div>
+        </Modal>
+      <div style={{width: "85vw", margin: "auto", padding: "10px", background: "#DCDCDC"}} >
+        <h3>This JSON is for testing purposes</h3>
+        <code style={{overflowWrap: "break-word"}} >{JSON.stringify(topic)}</code>
+      </div>
+    </div>
   )
 }
 
