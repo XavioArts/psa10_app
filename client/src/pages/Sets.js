5813392d4028fe1e1b01a3944eb17561b763161a
Fabio Xavier <fabio.g.xavier33@gmail.com>
2022-01-19 22:09:56 -0700

    Search by sets functionality

--- client/src/pages/Sets.js
+++ client/src/pages/Sets.js
@@ -1,10 +1,12 @@
 import { Button, Icon, Input, Stack } from "@mui/material";
 import { Box } from "@mui/system";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../providers/AuthProvider";
 
 const Sets = () => {
 
+    const auth = useContext(AuthContext)
     const [cards, setCards] = useState(null);
     const [search, setSearch] = useState("");
 
@@ -17,7 +19,7 @@ const Sets = () => {
     const searchCards = async (e) => {
         e.preventDefault();
         try {
-            let res = await axios.get(`/api/cards/search/${search}`);
+            let res = await axios.get(`/api/cards/${auth.id}/search/${search}`);
             setCards(res.data);
         } catch (err) {
             console.log(err.response);
@@ -46,10 +48,13 @@ const Sets = () => {
                 </Stack>
             </div>
             <hr/>
-            <Box sx={{width: "80vw", bgcolor: "#D7D7D7", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", margin: "auto", padding: "20px"}} >
+            {!cards && <Box sx={{width: "80vw", bgcolor: "#D7D7D7", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", margin: "auto", padding: "20px"}} >
                 <Icon sx={{fontSize:"150px"}} >pageview</Icon>
                 <p>There are no cards to diplay, please search for a set..</p>
-            </Box>
+            </Box>}
+            {cards && <Box sx={{width: "80vw", bgcolor: "#D7D7D7", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", margin: "auto", padding: "20px"}} >
+                <code>{JSON.stringify(cards)}</code>
+            </Box>}
         </div>
     )
 };
