be4c714b5d6fd6a729970e3bfcac2643cd232f34
Austin <austinchoi22@gmail.com>
2022-01-27 15:16:37 -0700

    card likes pulling from master

--- client/src/components/CardLike.js
+++ client/src/components/CardLike.js
@@ -5,37 +5,49 @@ import { AuthContext } from '../providers/AuthProvider';
 
 const CardLike = (props) => {
   const auth = useContext(AuthContext);
-  const [liked, setLiked] = useState(false);
+  const [liked, setLiked] = useState(auth.liked_cards.includes(props.card.id) ? true : false);
   const [like, setLike] = useState(props.card.likes)
-  console.log(props.card.likes)
-  console.log(auth)
+  let likedArray = auth.liked_cards
+  console.log(props.card)
+  console.log(auth.liked_cards)
+  console.log(liked)
 
   useEffect(() => {
-    cardLiked();
-  }, [])
+    getUser();
+  },[]);
 
-  const cardLiked = () => {
-    if (auth.liked_cards.includes(props)) {
-      setLiked(true)
-    }
+  const getUser = async () => {
+    let res = await axios.get(`/api/users/info/${auth.id}`)
   }
 
-  const handleLike = () => {
+  const handleLike = async () => {
     if (!liked) {
-      setLiked(true)
+      setLiked(true);
+      likedArray.push(props.collection.id)
       let liked = like + 1
+      let updatedCardLikes = await axios.put(`/api/users/liked_cards`, {liked_cards: likedArray})
+      let updateCardLikes = await axios.put(`/api/cards/${props.card.id}, {likes: liked}`)
+      console.log(updateUserLikes.data)
       setLike(liked)
     }
     else {
-      setLiked(false)
-      let unLiked = like - 1
-      setLike(unLiked)
+      setLiked(false);
+      likedArray = likedArray.filter((c) => props.card.id !== c)
+      console.log(likedArray)
+      let unliked = like - 1
+      let removeUserLikes = await axios.put(`/api/users/liked_card`, {liked_cards: likedArray})
+      let removeCardLikes = await axios.put(`api/cards/${props.card.id}`, {likes: unLiked})
+      console.log(removeUserLikes.data)
+      setLike(unliked)
     }
   }
 
   return (
-    <button onClick={() => { handleLike() }}> {liked === false ? <FavoriteBorderIcon /> : <FavoriteIcon />}{like}</button >
-  );
-}
+    <button onClick={() => {handleLike()}}>
+      
+    </button>
+
+    
+    )
 
 export default CardLike;
