3b1f205ee15385a957beedcab2ac1956eb20e13e
John Leosco <jrleosco@gmail.com>
2022-01-28 02:58:23 -0700

    Welcome Image test working

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,12 +1,25 @@
 import { Button } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
+import Carousel from '../components/Carousel';
 
 const Welcome = (props) => {
+
+  const images = [
+    <img src={'https://www.jotform.com/blog/wp-content/uploads/2020/01/Productivity-featured.png'}/>,
+    <img src={'https://www.jotform.com/blog/wp-content/uploads/2020/01/Productivity-featured.png'}/>,
+    <img src={'https://www.jotform.com/blog/wp-content/uploads/2020/01/Productivity-featured.png'}/>,
+    <img src={'https://www.jotform.com/blog/wp-content/uploads/2020/01/Productivity-featured.png'}/>,
+    <img src={'https://www.jotform.com/blog/wp-content/uploads/2020/01/Productivity-featured.png'}/>
+  ]
+
   return(
     <Box sx={{...style}}>
       <h1>Welcome to PSA10</h1>
       <p>Let's get started building you digital collection</p>
+      <Carousel show={1} infiniteLoop={false} style={{margin: '10px'}}>
+        {images}
+      </Carousel>
       <Button onClick={props.handleClose}>Close Modal</Button>
     </Box>
   )
