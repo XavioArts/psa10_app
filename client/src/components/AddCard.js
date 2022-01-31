642481cbca7f02ff0c1f0423f9179fe7dd4bbbf6
Fabio Xavier <fabio.g.xavier33@gmail.com>
2022-01-31 00:10:18 -0700

    add card fixes and error handling

--- client/src/components/AddCard.js
+++ client/src/components/AddCard.js
@@ -1,7 +1,7 @@
-import { Alert, Autocomplete, Button, FormControl, FormControlLabel, FormHelperText, Input, InputLabel, LinearProgress, MenuItem, Paper, Radio, RadioGroup, Select, TextField } from "@mui/material";
+import { Alert, Autocomplete, Button, FormControl, FormControlLabel, FormHelperText, Input, InputLabel, LinearProgress, MenuItem, Paper, Radio, RadioGroup, Select, TextField, Tooltip } from "@mui/material";
 import { Box } from "@mui/system";
 import axios from "axios";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import CardImageUpload from "./CardImageUpload";
 
@@ -23,6 +23,17 @@ const AddCard = (props) => {
     const [available, setAvailable] = useState(false);
     const [graded, setGraded] = useState(false);
     const [grade, setGrade] = useState("");
+    const [submitted, setSubmitted] = useState(false);
+    const [upload, setUpload] = useState(false);
+    const [trueSubmit, setTrueSubmit] = useState(false)
+    const [notUploaded, setNotUploaded] = useState(false);
+
+    useEffect(()=>{
+        console.log("true",trueSubmit)
+        if(upload && trueSubmit){
+            handleSubmit()
+        }
+    },[upload]);
 
     const categories = [
         {name: 'Pokemon', value: "Pokemon", subCategory: "Trading Card Games"},
@@ -75,28 +86,60 @@ const AddCard = (props) => {
         }
     };
 
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     let updatedCard = {name, category: chosenCategory, condition: chosenCondition, set, year, card_number, available, grade, graded};
+    //     try {
+    //         let res = await axios.put(`/api/cards/${card.id}`, updatedCard)
+    //         addCard(res.data);
+    //         setCard(null);
+    //         setClicked(false);
+    //         setFailed(false);
+    //         //reset form values
+    //         setName("");
+    //         setCategory("");
+    //         setCondition("");
+    //         setSet("");
+    //         setYear(2022);
+    //         setCardNumber("");
+    //         setAvailable(false);
+    //         setSuccess(true);
+    //         setTimeout(()=>setSuccess(false), 6000);
+    //     } catch (err) {
+    //         console.log(err.response);
+    //         setFailed(true);
+    //     }
+    // }
+
     const handleSubmit = async (e) => {
-        e.preventDefault();
-        let updatedCard = {name, category: chosenCategory, condition: chosenCondition, set, year, card_number, available, grade, graded};
-        try {
-            let res = await axios.put(`/api/cards/${card.id}`, updatedCard)
-            addCard(res.data);
-            setCard(null);
-            setClicked(false);
-            setFailed(false);
-            //reset form values
-            setName("");
-            setCategory("");
-            setCondition("");
-            setSet("");
-            setYear(2022);
-            setCardNumber("");
-            setAvailable(false);
-            setSuccess(true);
-            setTimeout(()=>setSuccess(false), 6000);
-        } catch (err) {
-            console.log(err.response);
-            setFailed(true);
+        if(e){e.preventDefault();}
+        if(upload===true){
+            let updatedCard = {name, category: chosenCategory, condition: chosenCondition, set, year, card_number, available, grade, graded};
+            try {
+                let res = await axios.put(`/api/cards/${card.id}`, updatedCard)
+                addCard(res.data);
+                setCard(null);
+                setClicked(false);
+                setFailed(false);
+                //reset form values
+                setName("");
+                setCategory("");
+                setCondition("");
+                setSet("");
+                setYear(2022);
+                setCardNumber("");
+                setAvailable(false);
+                setSuccess(true);
+                setUpload(false)
+                setTrueSubmit(false)
+                setTimeout(()=>setSuccess(false), 6000);
+                if(submitted === true){
+                    setSubmitted(false)
+                }
+            } catch (err) {
+                console.log(err.response);
+                setFailed(true);
+            }
         }
     }
 
@@ -134,6 +177,15 @@ const AddCard = (props) => {
         }
     };
 
+    const finished = () =>{
+        if (!upload) {
+            setNotUploaded(true);
+            return
+        }
+        setSubmitted(true)
+        setTrueSubmit(true)
+    }
+
     return(
         <div>
             {success && <Alert severity="success" >Successfuly created a new collectible!</Alert>}
@@ -147,10 +199,10 @@ const AddCard = (props) => {
                 <div>
                     <Paper sx={{width: "85vw", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", paddingBottom: "20px"}} >
                     <h4>Please upload card images and then fill out card info</h4>
-                    <CardImageUpload id={card.id} />
+                    <CardImageUpload id={card.id} submitted={submitted} setSubmitted={setSubmitted} setUpload={setUpload} />
                         <form onSubmit={handleSubmit} > 
                             <label>Name: </label>
-                            <Input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
+                            <Input type="text" value={name} required onChange={(e)=>setName(e.target.value)} />
                             <FormControl sx={{ m:1, minWidth: 250}} >
                                 <Autocomplete 
                                     options={categories}
@@ -161,7 +213,7 @@ const AddCard = (props) => {
                                     onChange={(e, newValue) => setCategory(newValue)}
                                     inputValue={chosenCategory}
                                     onInputChange={(e, newValue) => setChosenCategory(newValue)}
-                                        
+                                    required
                                 />
                             </FormControl>
                             <FormControl sx={{ m:1, minWidth: 250}} >
@@ -173,16 +225,16 @@ const AddCard = (props) => {
                                     onChange={(e, newValue) => setCondition(newValue)}
                                     inputValue={chosenCondition}
                                     onInputChange={(e, newValue) => setChosenCondition(newValue)}
-                                        
+                                    required
                                 />
                             </FormControl>
                             <br/>
                             <label>Set: </label>
-                            <Input type="text" value={set} onChange={(e)=>setSet(e.target.value)} />
+                            <Input type="text" required value={set} onChange={(e)=>setSet(e.target.value)} />
                             <label>Year: </label>
-                            <Input type="number" value={year} onChange={(e)=>setYear(e.target.value)} />
+                            <Input type="number" required value={year} onChange={(e)=>setYear(e.target.value)} />
                             <label>Card No.: </label>
-                            <Input type="text" value={card_number} onChange={(e)=>setCardNumber(e.target.value)} />
+                            <Input type="text" required value={card_number} onChange={(e)=>setCardNumber(e.target.value)} />
                             <br/>
                             <div style={{display: "flex", alignItems: "center", justifyContent: "space-evenly"}} >
                                 <div>
@@ -306,7 +358,9 @@ const AddCard = (props) => {
                                     </Select>
                                 </FormControl>
                             </div>
-                            <Button variant="contained" type="submit" >Submit</Button>
+                            <Tooltip open={notUploaded} onClose={()=>setNotUploaded(false)} title="Please upload your images" >
+                            <Button variant="contained" type="submit" onClick={()=>finished()} >Submit</Button>
+                            </Tooltip>
                         </form>
                         <Button variant="contained" color="error" onClick={deleteCard} >Cancel</Button>
                     </Paper>
