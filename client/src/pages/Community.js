66135fc2e9bc2bc39939e9da7b304cb2eb7fbaf5
Fabio Xavier <fabio.g.xavier33@gmail.com>
2022-01-30 20:38:47 -0700

    community fixes

--- client/src/pages/Community.js
+++ client/src/pages/Community.js
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, Icon, IconButton, Input, List, ListItem, ListItemAvatar, ListItemText, Stack } from "@mui/material";
+import { Avatar, Box, Button, Icon, IconButton, Input, List, ListItem, ListItemAvatar, ListItemText, Stack, Tooltip } from "@mui/material";
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -12,6 +12,7 @@ const Community = () => {
     const [users, setUsers] = useState([]);
     const [search, setSearch] = useState("");
     const [filteredUsers, setFilteredUsers] = useState([]);
+    const [emptySearch, setEmptySearch] = useState(false);
     const navigate = useNavigate();
 
     useEffect(()=>{
@@ -31,6 +32,10 @@ const Community = () => {
 
     const searchUsers = async (e) => {
         e.preventDefault();
+        if (search === "") {
+            setEmptySearch(true);
+            return 
+        }
         try {
             let res = await axios.get(`/api/users/search/${search}`);
             setFilteredUsers(res.data);
@@ -49,13 +54,14 @@ const Community = () => {
                     <IconButton onClick={()=>navigate(`/community/users/${u.id}/profile`)} >
                         <Icon>visibility</Icon>
                     </IconButton>
+                    <label>Socials</label>
                     <UserContactModal {...u} />
                     </>
                 } >
-                    <ListItemAvatar>
+                    <ListItemAvatar onClick={()=>navigate(`/community/users/${u.id}/profile`)} >
                         <Avatar alt="profile" src={u.image} />
                     </ListItemAvatar>
-                    <ListItemText primary={u.nickname} secondary={`${u.first_name} ${u.last_name}`} />
+                    <ListItemText onClick={()=>navigate(`/community/users/${u.id}/profile`)} primary={u.nickname} secondary={`${u.first_name} ${u.last_name}`} />
                 </ListItem>
             )
         })
@@ -77,13 +83,15 @@ const Community = () => {
             </div>
             <div style={{width: "75vw", margin: "auto", padding: "10px", display: "flex", justifyContent: "center", alignItems: "center"}} >
                 <div style={{width: "60vw", margin: "10px"}} >
-                <Input 
-                fullWidth 
-                startAdornment={<Icon>search</Icon>} 
-                placeholder="Search by name, username, or email.." 
-                value={search}
-                onChange={(e)=>{setSearch(e.target.value)}}
-                type="search" />
+                <Tooltip open={emptySearch} onClose={()=>setEmptySearch(false)} title="Please enter a search term" >
+                    <Input 
+                    fullWidth 
+                    startAdornment={<Icon>search</Icon>} 
+                    placeholder="Search by name, username, or email.." 
+                    value={search}
+                    onChange={(e)=>{setSearch(e.target.value)}}
+                    type="search" />
+                </Tooltip>
                 </div>
                 <Stack spacing={1} direction="row" >    
                     <Button onClick={searchUsers} variant="contained" >Search</Button>
