01e313297053fb2409d22a0d915af9b2f1e775e1
Austin <austinchoi22@gmail.com>
2022-02-01 15:46:31 -0700

    footer fixed to bottom

--- client/src/App.js
+++ client/src/App.js
@@ -36,68 +36,72 @@ import Welcome from './pages/Welcome';
 import SelectCollection from './components/SelectCollection';
 import SearchByName from './pages/SearchByName';
 import Faq from './pages/Faq';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="page-container">
-      <div className="content-wrap">
+    <>
+      <div className="page-container">
+        <div className="content-wrap">
 
-        <div>
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/test" element={<Home />} />
-              <Route path="/about" element={<AboutUs />} />
-              <Route path="/messageboard" element={<MessageBoard />} />
-              <Route path="/messageboard/:id" element={<TopicPage />} />
-              <Route path="/login" element={<LogIn />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/welcome" element={<Welcome />} />
-              {/* The community section needs to not be in requireAuth because
+          <div>
+            <Routes>
+              <Route element={<Layout />}>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/test" element={<Home />} />
+                <Route path="/about" element={<AboutUs />} />
+                <Route path="/messageboard" element={<MessageBoard />} />
+                <Route path="/messageboard/:id" element={<TopicPage />} />
+                <Route path="/login" element={<LogIn />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/welcome" element={<Welcome />} />
+                {/* The community section needs to not be in requireAuth because
               it cant determine the difference in protected when both are in there */}
-              <Route path="/community" element={<Community />} />
-              <Route path="/community/users/:user_id/profile" element={<Protected />}>
-                <Route path="/community/users/:user_id/profile/collections" element={<UserCollections />} />
-                <Route path="/community/users/:user_id/profile/collections/:id" element={<UserCollectView />} />
-                <Route path="/community/users/:user_id/profile/showcases" element={<UserShowcases />} />
-                <Route path="/community/users/:user_id/profile/sets" element={<UserSets />} />
-                <Route path="/community/users/:user_id/profile" element={<UserOverview />} />
-              </Route>
-          {/* All routes below here are required to be logged in */}
-          <Route element={<RequireAuth/>} >
-
-                <Route path="/profile_image" element={<ProfileImageUpload />} />
-                <Route path="/profile/edit_card/:id" element={<EditCard />} />
-                <Route path="/upload" element={<UploadCollectible />} />
-                <Route path="/search/:search" element={<SearchByName />} />
-
-                {/* All routes nested in here will be sub pages to the profile */}
-                <Route path="/profile" element={<Protected />}>
-                  <Route path="/profile/collections" element={<Collections />} />
-                  <Route path="/profile/collections/:id" element={<Collection />} />
-                  <Route path="/profile/collections/:id/edit" element={<EditCollection />} />
-                  <Route path="/profile/showcases" element={<Showcase />} />
-                  <Route path="/profile/showcases" element={<Showcases />} />
-                  <Route path="/profile/overview" element={<Overview />} />
-                  <Route path="/profile/showcases/:id/edit" element={<ShowcaseEdit />} />
-                  <Route path="/profile/sets" element={<Sets />} />
+                <Route path="/community" element={<Community />} />
+                <Route path="/community/users/:user_id/profile" element={<Protected />}>
+                  <Route path="/community/users/:user_id/profile/collections" element={<UserCollections />} />
+                  <Route path="/community/users/:user_id/profile/collections/:id" element={<UserCollectView />} />
+                  <Route path="/community/users/:user_id/profile/showcases" element={<UserShowcases />} />
+                  <Route path="/community/users/:user_id/profile/sets" element={<UserSets />} />
+                  <Route path="/community/users/:user_id/profile" element={<UserOverview />} />
                 </Route>
+                {/* All routes below here are required to be logged in */}
+                <Route element={<RequireAuth />} >
 
-                <Route path="/profile/cover_image" element={<CoverImgUpload />} />
-                <Route path="/api/users/:id/edit" element={<EditProfileForm />} />
-                <Route path="/showcase/new" element={<ShowcaseNewForm />} />
+                  <Route path="/profile_image" element={<ProfileImageUpload />} />
+                  <Route path="/profile/edit_card/:id" element={<EditCard />} />
+                  <Route path="/upload" element={<UploadCollectible />} />
+                  <Route path="/search/:search" element={<SearchByName />} />
 
-                <Route path="/collection/new" element={<CollectionNew />} />
-                <Route path="/cards/:id/image" element={<CardImageUpload />} />
-                <Route path="/users/:id/edit" element={<EditProfileForm />} />
-                <Route path="/select_collection" element={<SelectCollection/>}/>
-                <Route path="/faq" element={<Faq/>}/>
+                  {/* All routes nested in here will be sub pages to the profile */}
+                  <Route path="/profile" element={<Protected />}>
+                    <Route path="/profile/collections" element={<Collections />} />
+                    <Route path="/profile/collections/:id" element={<Collection />} />
+                    <Route path="/profile/collections/:id/edit" element={<EditCollection />} />
+                    <Route path="/profile/showcases" element={<Showcase />} />
+                    <Route path="/profile/showcases" element={<Showcases />} />
+                    <Route path="/profile/overview" element={<Overview />} />
+                    <Route path="/profile/showcases/:id/edit" element={<ShowcaseEdit />} />
+                    <Route path="/profile/sets" element={<Sets />} />
+                  </Route>
+
+                  <Route path="/profile/cover_image" element={<CoverImgUpload />} />
+                  <Route path="/api/users/:id/edit" element={<EditProfileForm />} />
+                  <Route path="/showcase/new" element={<ShowcaseNewForm />} />
+
+                  <Route path="/collection/new" element={<CollectionNew />} />
+                  <Route path="/cards/:id/image" element={<CardImageUpload />} />
+                  <Route path="/users/:id/edit" element={<EditProfileForm />} />
+                  <Route path="/select_collection" element={<SelectCollection />} />
+                  <Route path="/faq" element={<Faq />} />
+                </Route>
               </Route>
-            </Route>
-          </Routes>
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
