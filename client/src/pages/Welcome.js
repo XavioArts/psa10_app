0bd0f2c06a8feaf5ed6099a8d99188ad4a5576ca
John Leosco <jrleosco@gmail.com>
2022-02-02 18:22:38 -0700

    welcome complete

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -4,11 +4,10 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Carousel from '../components/Carousel';
 import Slide1 from '../slides/slide1.mov'
-import Slide2 from '../slides/slide2.png'
-import Slide3 from '../slides/slide3.png'
-import Slide4 from '../slides/slide4.png'
-import Slide5 from '../slides/slide5.png'
-import Slide6 from '../slides/slide6.png'
+import Slide2 from '../slides/slide2.mov'
+import Slide3 from '../slides/slide3.mov'
+import Slide4 from '../slides/slide4.mov'
+import Slide5 from '../slides/slide5.mov'
 
 const Welcome = (props) => {
   const navigate = useNavigate();
@@ -32,9 +31,8 @@ const Welcome = (props) => {
     {id: 1, image: Slide1, text: 'First let head over to you collections and create you first collection'},
     {id: 2, image: Slide2, text: 'Start by clicking the "Add a Collection" button'},
     {id: 3, image: Slide3, text: 'Next give your collection a name, a category, and a description'},
-    {id: 4, image: Slide4, text: 'Now click on the name of you collection'},
-    {id: 5, image: Slide5, text: 'Lets add your first collectable'},
-    {id: 6, image: Slide6, text: "Finally fill out the collectable's info and add it to your collection"},
+    {id: 5, image: Slide4, text: 'Lets add your first collectable'},
+    {id: 6, image: Slide5, text: "Finally fill out the collectable's info and add it to your collection"},
     {id: 7, image: 'https://i.pinimg.com/originals/9e/bf/a7/9ebfa7a509fb8a748c61497ac95a0ac3.gif', text: 'Great job!! Now your free to add more collectables, explore showcases, make new friends, and organize trades!!'}
   ]
 
@@ -44,7 +42,7 @@ const Welcome = (props) => {
         <div key={s.id}>
           <h3>{s.text}</h3>
           <br />
-          <video style={{ width: `900px`, height: `460px`}} autoPlay muted>
+          <video style={{ width: `900px`, height: `460px`}} autoPlay loop muted>
             <source src={s.image} type="video/mp4"></source>
         </video>
         </div>
